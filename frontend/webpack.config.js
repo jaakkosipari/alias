ea0dbcf44f7a4251ad9d3d95e2539ac7d6f1f7cb
jaakkosipari <jaakko.sipari@gmail.com>
2024-10-20 14:52:32 +0300

    Fix webpack dev server to serve index.html
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/jaakkosipari/alias?shareId=XXXX-XXXX-XXXX-XXXX).

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -22,6 +23,11 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
